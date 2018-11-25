b32ee55ec6a334cb83aac5cf65e1abe0b2fb6793
lukearmstrongkc <taurusstrongarm@gmail.com>
2018-11-25 15:22:10 -0600

    Added secondary table for when database is linked to - commented out until ready to use as it breaks the page without a DB to call from.

--- static/js/app.js
+++ static/js/app.js
@@ -2,12 +2,11 @@
 var tableData = data;
 
 // Step 1 - Use d3 to set up the table body and give it a variable.
-var tbody = d3.select("tbody");
+var tbody = d3.select("#tbody1");
 
 console.log(tableData);
 
-// Step 2 - Use d3 to update/append cell's text with UFO data
-//  such as Date/City/State/Country/Shape/Duration/Comment
+// Step 2 - Use d3 to update/append cell's text with Song data such as Song, Artist, and Album
 function displayData(data){
   tbody.text("")
   data.forEach(function(view){
@@ -19,50 +18,14 @@ function displayData(data){
 
 displayData(tableData)
 
+var inputSong = d3.select("#songname");
 
-var button = d3.select("filter-btn");
-var inputDate = d3.select("#datetime");
-
-
-var inputCity = d3.select("#city");
-var inputState = d3.select("#state");
-var inputCountry = d3.select("#country");
-var inputShape = d3.select("#shape");
-
-function updateDates() {
+function updateSongs() {
   d3.event.preventDefault();
-  console.log(inputDate.property("value"));
-  newDates = tableData.filter(date => date.datetime===inputDate.property("value"))
-  displayData(newDates)
+  console.log(inputSong.property("value"));
+  newSongs = tableData.filter(song => song.songname===inputSong.property("value"))
+  displayData(newSongs)
 }
-function updateCity() {
-  d3.event.preventDefault();
-  console.log(inputCity.property("value"));
-  newCity = tableData.filter(city => city.city===inputCity.property("value"))
-  displayData(newCity)
-}
-function updateState() {
-  d3.event.preventDefault();
-  console.log(inputState.property("value"));
-  newState = tableData.filter(state => state.state===inputState.property("value"))
-  displayData(newState)
-}
-function updateCountry() {
-  d3.event.preventDefault();
-  console.log(inputCountry.property("value"));
-  newCountry = tableData.filter(country => country.country===inputCountry.property("value"))
-  displayData(newCountry)
-}
-function updateShape() {
-  d3.event.preventDefault();
-  console.log(inputShape.property("value"));
-  newShape = tableData.filter(shape => shape.shape===inputShape.property("value"))
-  displayData(newShape)
-}
-inputDate.on("change",updateDates)
-inputCity.on("change",updateCity);
-inputState.on("change",updateState);
-inputCountry.on("change",updateCountry);
-inputShape.on("change",updateShape);
+inputSong.on("change",updateSongs)
 
 button.on("click", displayData)
--- static/js/data.js
+++ static/js/data.js
@@ -1,5 +1,10 @@
 var data = [{
-  songtitle: "Push It",
+  songname: "Push It",
   artist: "Salt'N'Peppa",
+  album: "Hot, Cool & Vicious"
+  },
+  {songname: "Round Up",
+  artist: "Sam Spence",
+  album:"Autumn Thunder"
   },
 ];
