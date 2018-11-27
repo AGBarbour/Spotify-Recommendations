e49ede3e6a72cc8d7ac4245936465400bfa900f8
Evan Wilson <chromevalkierie.evan@gmail.com>
2018-11-26 19:35:59 -0600

    app2.js updates and added name attribute to html

--- static/js/app2.js
+++ static/js/app2.js
@@ -1,45 +1,51 @@
 // from data.js
-var suggestData = data;
-// var recommendedData = dbData
 // Step 1 - Use d3 to set up the table body and give it a variable.
 var tbody = d3.select("#tbody1");
 var tbody2 = d3.select("#tbody2");
 
-console.log(suggestData);
+// console.log(suggestData);
 
 // Step 2 - Use d3 to update/append cell's text with song data such as Title, Artist, and Album
-function displayData(data){
-  tbody.text("")
-  data.forEach(function(view){
-    row = tbody.append("tr")
-    Object.entries(view).forEach(function([key,value]) {
-      cell = row.append("td").text(value)
+function displayData(data) {
+  tbody.text("");
+  data.forEach(function (view) {
+    row = tbody.append("tr");
+    Object.entries(view).forEach(function ([key, value]) {
+      cell = row.append("td").text(value);
     });
-  })};
+  });
+}
 
-displayData(suggestData)
+// displayData(suggestData);
 
 // var button = d3.select("filter-btn");
 var inputSong = d3.select("#songname");
 
+// function to call flask api and update the second table with recommendations
+function displayDB(dbData) {
+  tbody2.text("");
+  var recommendedData = d3.json(`/recommend`);
+  recommendedData.forEach(function (view) {
+    row = tbody2.append("tr");
+    Object.entries(view).forEach(function ([key, value]) {
+      cell = row.append("td").text(value);
+    });
+  });
+}
 
-// function displayDB(dbData){
-//   tbody2.text("")
-//   dbData.forEach(function(view){
-//     row = tbody2.append("tr")
-//     Object.entries(view).forEach(function([key,value]) {
-//       cell = row.append("td").text(value)
-//     });
-//   })};
+// displayDB(recommendedData);
 
-// displayDB(recommendedData)
-
+// function to update the first table with the users search result
 function updateRecs() {
   d3.event.preventDefault();
+  var suggestData = d3.json(`/search`);
   console.log(inputSong.property("value"));
-  newSongs = suggestData.filter(song => song.songname===inputSong.property("value"))
-  displayData(newSongs)
+  newSongs = suggestData.filter(song => song.songname === inputSong.property("value"));
+  displayData(newSongs);
 }
-inputSong.on("change",updateRecs);
 
-button.on("click", displayData);
+// listener to update table one when the user types a song
+inputSong.on("change", updateRecs);
+
+// listener to update table two when the user clicks the button to recommend songs
+button.on("click", displayDB);
